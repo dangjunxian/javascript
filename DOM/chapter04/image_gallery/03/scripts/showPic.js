b83c20475b85ccae4babe44b179179f1ee2145d3
dangjunxian <32083521+dangjunxian@users.noreply.github.com>
2017-09-19 18:08:08 +0800

    添加了第三版

--- DOM/chapter04/image_gallery/03/scripts/showPic.js
+++ DOM/chapter04/image_gallery/03/scripts/showPic.js
@@ -1,8 +1,19 @@
-function showPic(whichpic) {
+function showPic1(whichpic) {
   var source = whichpic.getAttribute("href");
   var placeholder = document.getElementById("placeholder");
   placeholder.setAttribute("src",source);
   var text = whichpic.getAttribute("title");
   var description = document.getElementById("description");
   description.firstChild.nodeValue = text;
+}
+
+
+function showPic(whichpic)
+{
+	var x = whichpic.getAttribute("href");
+	var y = document.getElementById("placeholder");
+	y.setAttribute("src",x);
+	var z = whichpic.getAttribute("title");
+    var jieshao = document.getElementById("description");
+    jieshao.firstChild.nodeValue = z;
 }
