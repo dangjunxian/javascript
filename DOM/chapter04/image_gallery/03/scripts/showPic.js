7c2a251c83e5b8b30d5dcfc72d95b59ee0e19457
dangjunxian <32083521+dangjunxian@users.noreply.github.com>
2017-09-20 18:08:26 +0800

    添加第四种
    css 和 js 代码与html文件分离

--- DOM/chapter04/image_gallery/03/scripts/showPic.js
+++ DOM/chapter04/image_gallery/03/scripts/showPic.js
@@ -13,7 +13,11 @@ function showPic(whichpic)
 	var x = whichpic.getAttribute("href");
 	var y = document.getElementById("placeholder");
 	y.setAttribute("src",x);
+	//第二种替换图片的方法：
+	//document.getElementById("placeholder").src=x;
 	var z = whichpic.getAttribute("title");
     var jieshao = document.getElementById("description");
     jieshao.firstChild.nodeValue = z;
+	//第二种替换下面文字的方法：
+	//document.getElementById("description").innerHTML= z;
 }
